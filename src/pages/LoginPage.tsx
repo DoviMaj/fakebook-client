c6dab68721b2eb13007014ce6055f9bada077060
dovimaj <dovimajowka@gmail.com>
2021-02-26 07:12:17 +0200

    Some clean up for LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./LoginPage.scss";
 
-function LoginPage(props: any) {
+function LoginPage() {
   return (
     <div className="login-wrapper">
       <div className="login-form">
         <div>
           <p className="logo">fakebook</p>
         </div>
+        <div></div>
         <a className="link-button" href="http://localhost:5000/auth/facebook">
           Sign-in with Facebook
+        </a>
+        <a className="link-button" href="http://localhost:5000/auth/google">
+          Sign-in with Google
         </a>
       </div>
     </div>
