811d0687bbbd94d30d35930fd84936fcffeba487
dovimaj <dovimajowka@gmail.com>
2021-02-28 12:00:54 +0200

    Post date formating

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import CommentForm from "./CommentForm";
 import "./Post.scss";
+import differenceInMinutes from "date-fns/differenceInMinutes";
+import differenceInHours from "date-fns/differenceInHours";
 
-const Post = ({ post }: any) => {
+const Post = ({ post, user }: any) => {
   const now = new Date();
-  const nowHrs = now.getHours();
-  const nowMinutes = now.getMinutes();
-  const postHour = new Date(post.date).getHours();
-  const postMin = new Date(post.date).getMinutes();
-  const hourDiff = nowHrs - postHour;
-  const minutesDiff = postMin - nowMinutes;
+  const postDate = Date.parse(post.date);
+  const diffHrs = differenceInHours(now, postDate);
+  const diffMins = differenceInMinutes(now, postDate);
   const fullDate = new Date(post.date).toLocaleDateString("en-gb", {
     month: "long",
     day: "numeric",
@@ -17,22 +17,19 @@ const Post = ({ post }: any) => {
     minute: "2-digit",
   });
   const displayDate =
-    hourDiff <= 1
-      ? `${minutesDiff}m`
-      : hourDiff > 24
-      ? fullDate
-      : `${hourDiff}hr`;
+    diffHrs < 1 ? `${diffMins}m` : diffHrs > 24 ? fullDate : `${diffHrs}hr  `;
   return (
     <div className="post">
       <div className="post-profile">
         <img src={post.User.picture_url}></img>
-        <div className="">
+        <div>
           <Link to="/profile">{post.User.username}</Link>
           <p>{displayDate}</p>
         </div>
       </div>
 
       <p>{post.text}</p>
+      <CommentForm user={user} />
     </div>
   );
 };
