1ea515afd2f1737c45b9db1a65ebf6b3a1f829a4
dovimaj <dovimajowka@gmail.com>
2021-03-01 20:07:16 +0200

    CommentList initial

---
+++ src/components/CommentList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./CommentList.scss";
+
+const CommentList = ({ comments }: any) => {
+  return (
+    <div className="comments-list">
+      {comments.map((comment: any) => {
+        return (
+          <div className="comment-wrapper">
+            <img src={comment.User.picture_url}></img>
+            <div className="comment-text">
+              <p className="username">{comment.User.username}</p>
+              <p>{comment.text}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CommentList;
