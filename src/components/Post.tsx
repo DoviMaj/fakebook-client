f978c377db3a7c1e4fc704d836afadeb8216e4c1
dovimaj <dovimajowka@gmail.com>
2021-03-01 20:06:43 +0200

    Display date clean up

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,23 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CommentForm from "./CommentForm";
 import "./Post.scss";
 import differenceInMinutes from "date-fns/differenceInMinutes";
 import differenceInHours from "date-fns/differenceInHours";
+import CommentList from "./CommentList";
 
 const Post = ({ post, user }: any) => {
   const now = new Date();
-  const postDate = Date.parse(post.date);
+  const postDate = new Date(Date.parse(post.date));
   const diffHrs = differenceInHours(now, postDate);
   const diffMins = differenceInMinutes(now, postDate);
-  const fullDate = new Date(post.date).toLocaleDateString("en-gb", {
+  const fullDate = postDate.toLocaleDateString("en-gb", {
     month: "long",
     day: "numeric",
     hour: "numeric",
     minute: "2-digit",
   });
   const displayDate =
-    diffHrs < 1 ? `${diffMins}m` : diffHrs > 24 ? fullDate : `${diffHrs}hr  `;
+    diffMins < 5
+      ? `Just now`
+      : diffHrs < 1
+      ? `${diffMins}m`
+      : diffHrs > 24
+      ? fullDate
+      : `${diffHrs}h`;
   return (
     <div className="post">
       <div className="post-profile">
@@ -29,7 +36,8 @@ const Post = ({ post, user }: any) => {
       </div>
 
       <p>{post.text}</p>
-      <CommentForm user={user} />
+      <CommentForm postId={post._id} user={user} />
+      <CommentList comments={post.comments} />
     </div>
   );
 };
