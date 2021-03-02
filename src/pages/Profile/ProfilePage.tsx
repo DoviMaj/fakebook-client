f49be092cecd96428641cf7608d0414453bade6f
dovimaj <dovimajowka@gmail.com>
2021-03-03 01:52:31 +0200

    Profile page initial setup

---
+++ src/pages/Profile/ProfilePage.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+
+const ProfilePage = () => {
+  useEffect(() => {
+    document.title = "Fakebook | Profile";
+  }, []);
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default ProfilePage;
