b31182d195b15e31a073d09276667c37f00da707
dovimaj <dovimajowka@gmail.com>
2021-03-04 22:46:25 +0200

    Fetch users for friends page

--- src/pages/Friends/FriendsPage.tsx
+++ src/pages/Friends/FriendsPage.tsx
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import { userContext } from "../../GlobalContext";
+import "./FriendsPage.scss";
 
 const FriendsPage = () => {
+  const currentUser = useContext(userContext);
+  const [notFriends, setNotFriends] = useState([] as any);
+
   useEffect(() => {
+    const getUsers = async () => {
+      const req = await fetch("http://localhost:5000/api/notFriends", {
+        credentials: "include",
+      });
+      setNotFriends(await req.json());
+    };
+    getUsers();
     document.title = "Fakebook | Friends";
   }, []);
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+
+      <div className="friends">
+        <div className="side-bar">
+          {notFriends &&
+            notFriends.map((friend: any) => <li>{friend.username}</li>)}
+        </div>
+      </div>
+    </>
   );
 };
 
