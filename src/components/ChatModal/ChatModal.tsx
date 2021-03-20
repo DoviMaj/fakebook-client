11ce9bbcdee099a5aad2ea7a9bda5e78a59bd7c5
dovimaj <dovimajowka@gmail.com>
2021-03-21 00:12:06 +0200

    Chat initial implementation

--- src/components/ChatModal/ChatModal.tsx
+++ src/components/ChatModal/ChatModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatModal.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
@@ -6,9 +6,34 @@ import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
 type Props = {
   targetUser: UserType | undefined;
   toggleDisplayModal: () => void;
+  currentUser: UserType | undefined;
+  socket: any;
+  chat: string[];
+  setChat: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
-const ChatModal: React.FC<Props> = ({ targetUser, toggleDisplayModal }) => {
+const ChatModal: React.FC<Props> = ({
+  targetUser,
+  toggleDisplayModal,
+  currentUser,
+  socket,
+  chat,
+  setChat,
+}) => {
+  const [input, setInput] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input !== "") {
+      setChat([...chat, input]);
+      socket.emit("send message", {
+        to: targetUser?._id,
+        from: currentUser!._id,
+        input,
+      });
+      setInput("");
+    }
+  };
+
   return (
     <div className="chat-modal">
       <div className="top">
@@ -25,6 +50,21 @@ const ChatModal: React.FC<Props> = ({ targetUser, toggleDisplayModal }) => {
         <strong>{targetUser?.username}</strong>
         <p>You're friends on Fakebook</p>
       </div>
+      {chat.map((msg, index) => {
+        return <li key={index}>{msg}</li>;
+      })}
+      <form className="chat-form" onSubmit={handleSubmit}>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          autoFocus
+          type="text"
+          autoComplete="off"
+          name="chat-message"
+          placeholder="Aa"
+          id=""
+        />
+      </form>
     </div>
   );
 };
--- src/components/ContactsBar/ContactBar.tsx
+++ src/components/ContactsBar/ContactBar.tsx
@@ -1,31 +1,61 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./ContactBar.scss";
 import { userContext } from "../../GlobalContext";
 import ChatModal from "../ChatModal/ChatModal";
+import { socket } from "../../socket";
 
 const ContactBar = () => {
   const currentUser = useContext(userContext);
   const [targetUser, setTargetUser] = useState<UserType | undefined>(undefined);
   const [displayModal, setDisplayModal] = useState(false);
-  const showModal = (friend: UserType) => {
+  const [chat, setChat] = useState<string[]>([]);
+
+  const chooseFriend = (friend: UserType) => {
     setTargetUser(friend);
-    toggleDisplayModal();
+    !displayModal && toggleDisplayModal();
   };
+
+  useEffect(() => {
+    setChat([]);
+  }, [targetUser]);
 
   const toggleDisplayModal = () => {
     setDisplayModal(!displayModal);
   };
+
+  useEffect(() => {
+    socket.on("recieve message", (msg: any) => {
+      msg.from === targetUser!._id && setChat([...chat, msg.input]);
+      console.log(msg, chat);
+    });
+
+    return () => {
+      socket.off("recieve message");
+    };
+  });
   return (
     <div className="contact-bar">
       <h3>Contacts</h3>
-      {currentUser?.friends.map((friend) => (
-        <div onClick={() => showModal(friend)} className="friend">
-          <img src={friend.picture_url} alt="" />
-          <p>{friend.username}</p>
-        </div>
-      ))}
+      {currentUser ? (
+        currentUser.friends.map((friend) => (
+          <div
+            key={friend._id}
+            onClick={() => chooseFriend(friend)}
+            className="friend"
+          >
+            <img src={friend.picture_url} alt="pic-url" />
+            <p>{friend.username}</p>
+          </div>
+        ))
+      ) : (
+        <div className="friend">No Contacts</div>
+      )}
       {displayModal && (
         <ChatModal
+          setChat={setChat}
+          chat={chat}
+          socket={socket}
+          currentUser={currentUser}
           targetUser={targetUser}
           toggleDisplayModal={toggleDisplayModal}
         />
