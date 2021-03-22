e80ac6b6e53873cecd13b88d9a9e9a9ecfcda281
dovimaj <dovimajowka@gmail.com>
2021-03-22 17:35:44 +0200

    Fix Eslint error
    useCallback for getPosts function

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Navbar from "../../components/Navbar/Navbar";
 import Post from "../../components/Post/Post/Post";
@@ -18,24 +18,7 @@ const ProfilePage: React.FC = () => {
   type ID = { id: string };
   let { id } = useParams<ID>();
 
-  useEffect(() => {
-    document.title = `Fakebook | ${currentUser?.username}`;
-    currentUser!._id === id ? setProfileUser(currentUser) : fetchUser();
-    getPosts();
-  }, [id]);
-
-  async function fetchUser() {
-    setLoading(true);
-    window.scrollTo({ top: 0, behavior: "smooth" });
-    const request = await fetch(`http://localhost:5000/api/profile/${id}`, {
-      credentials: "include",
-    });
-    const res = await request.json();
-    setProfileUser(res.user);
-    document.title = `Fakebook | ${res.user?.username}`;
-    setLoading(false);
-  }
-  async function getPosts() {
+  const getPosts = useCallback(async () => {
     setLoading(true);
     window.scrollTo({ top: 0, behavior: "smooth" });
     const req = await fetch(`http://localhost:5000/api/profile/${id}`, {
@@ -44,7 +27,24 @@ const ProfilePage: React.FC = () => {
     const res = await req.json();
     setPosts(res.posts);
     setLoading(false);
-  }
+  }, [id]);
+
+  useEffect(() => {
+    document.title = `Fakebook | ${currentUser?.username}`;
+    currentUser!._id === id ? setProfileUser(currentUser) : fetchUser();
+    getPosts();
+    async function fetchUser() {
+      setLoading(true);
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      const request = await fetch(`http://localhost:5000/api/profile/${id}`, {
+        credentials: "include",
+      });
+      const res = await request.json();
+      setProfileUser(res.user);
+      document.title = `Fakebook | ${res.user?.username}`;
+      setLoading(false);
+    }
+  }, [id, currentUser, getPosts]);
 
   return (
     <>
