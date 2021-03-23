b34fd55bff1bd247ee5538e5920e2959eeab4a1f
dovimaj <dovimajowka@gmail.com>
2021-03-23 23:30:09 +0200

    Move get chat and socket connect into useEffect

--- src/socket.js
+++ src/socket.js
@@ -4,25 +4,6 @@ export const socket = io("http://localhost:5000", {
   withCredentials: true,
 });
 
-const getCurrentUserId = async () => {
-  const req = await fetch("http://localhost:5000/api/me", {
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-
-  const user = await req.json();
-  return user._id;
-};
-
-socket.on("connect", async () => {
-  const currentUserId = await getCurrentUserId();
-  socket.emit("logged-in", currentUserId);
-  console.log("connected");
-});
-
 socket.on("disconnect", () => {
   console.log("disconnected");
 });
