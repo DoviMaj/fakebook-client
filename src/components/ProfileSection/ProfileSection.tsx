91f4ea348434a60063e408141550d877cf636e2c
dovimaj <dovimajowka@gmail.com>
2021-04-14 20:28:40 +0300

    Add fixed image to profile cover

--- src/components/ProfileSection/ProfileSection.tsx
+++ src/components/ProfileSection/ProfileSection.tsx
@@ -26,7 +26,7 @@ const ProfileSection = ({
       <img
         className="cover-pic"
         alt="cover-pic"
-        src="https://source.unsplash.com/1600x900"
+        src="https://images.unsplash.com/photo-1470364798856-7a74180378dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80"
       ></img>
       <div className="pic-and-name">
         {isCurrentUser && (
